44c7ba823ab0b226a131af6ac97460a0b7c37320
Ihor Tykholyz <ig.tiholiz@gmail.com>
2026-05-08 23:42:35 +0300

    Translate comments/reviews pages

--- src/i18n/locales/en/dynamic.ts
+++ src/i18n/locales/en/dynamic.ts
@@ -15,12 +15,24 @@ export const dynamic: readonly DynamicTranslationRule[] = [
     replacement: 'Final published: ',
   },
   {
+    pattern: />>エピソード(\d+)の感想一覧/g,
+    replacement: '>> Comments for episode $1',
+  },
+  {
+    pattern: /エピソード(\d+)/g,
+    replacement: 'Episode $1',
+  },
+  {
     pattern: /(\d{4})\/(\d{1,2})\/(\d{1,2})/g,
     replacement: (_match, year, month, day) => formatConfiguredDate(year, month, day),
   },
   {
     pattern: /(\d{4})年\s*(\d{1,2})月\s*(\d{1,2})日/g,
     replacement: (_match, year, month, day) => formatConfiguredDate(year, month, day),
+  },
+  {
+    pattern: /(\d{1,2})時\s*(\d{1,2})分/g,
+    replacement: '$1:$2',
   },
   {
     pattern: /エピソード\s*([\d\s～]+)\s*を表示中/g,
